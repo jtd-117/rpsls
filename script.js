bb5de797b1401e79e1e84f3c1260c7a686bd9095
Jude Thaddeau Data <78249269+jtd-117@users.noreply.github.com>
2023-06-27 21:49:43 +1000

    Add 'getComputerMove' function

--- script.js
+++ script.js
@@ -26,3 +26,44 @@ const gameResult = Object.freeze({
     Win: 1
 })
 /* -------------------------------------------------------------------------- */
+/**
+ * @description Randomly generates an INTEGER that represents the computer's
+ *              move according to the `moves` ENUM.
+ * @returns     {number} An ENUM from `moves`
+ */
+function getComputerMove() {
+
+    // STEP 1: Select the `computer-move` div
+    const computerMove = document.querySelector(`div[id="computer"] div[id="choice"]`);
+    computerMove.textContent = "Computer: ";
+
+    // STEP 2: Randomly select an integer
+    let result = Math.floor(Math.random() * 5);
+
+    // CASE 3A: Randomly selected ROCK
+    if (result === 0) {
+        computerMove.textContent += '✊';
+        return moveNames.Rock;
+
+    // CASE 3B: Randomly selcted PAPER
+    } else if (result === 1) {
+        computerMove.textContent += '🤚';
+        return moveNames.Paper;
+
+    // CASE 3C: Randomly selected SCIZZOR
+    } else if (result === 2) {
+        computerMove.textContent += '✌️';
+        return moveNames.Scizzor;
+
+    // CASE 3D: Randomly selected LIZARD
+    } else if (result === 3) {
+        computerMove.textContent += '🤏';
+        return moveNames.Lizard;
+    
+    // CASE 3E: Randomly selected SPOCK
+    } else {
+        computerMove.textContent += '🖖';
+        return moveNames.Spock;
+    }
+}
+/* -------------------------------------------------------------------------- */
