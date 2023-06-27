fcd6226a21e0fb1783e828c07aebc904dcda7a9f
Jude Thaddeau Data <78249269+jtd-117@users.noreply.github.com>
2023-06-27 22:09:40 +1000

    Add 'refreshPage' function

--- script.js
+++ script.js
@@ -192,3 +192,11 @@ function disableButtons() {
     });
 }
 /* -------------------------------------------------------------------------- */
+/**
+ * @description Refreshes the website.
+ * @link        https://www.freecodecamp.org/news/javascript-refresh-page-how-to-reload-a-page-in-js/#:~:text=The%20simplest%20way%20to%20refresh,and%20loading%20the%20latest%20content.
+ */
+function refreshPage() {
+    location.reload();
+}
+/* -------------------------------------------------------------------------- */
