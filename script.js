7124f256d5ac37232bc49037ac06f379e926dd8d
Jude Thaddeau Data <78249269+jtd-117@users.noreply.github.com>
2023-06-27 21:57:03 +1000

    Add 'getHumanMove' function

--- script.js
+++ script.js
@@ -33,37 +33,74 @@ const gameResult = Object.freeze({
  */
 function getComputerMove() {
 
-    // STEP 1: Select the `computer-move` div
+    // STEP 1: Select the computer's `choice` div
     const computerMove = document.querySelector(`div[id="computer"] div[id="choice"]`);
-    computerMove.textContent = "Computer: ";
 
     // STEP 2: Randomly select an integer
     let result = Math.floor(Math.random() * 5);
 
     // CASE 3A: Randomly selected ROCK
     if (result === 0) {
-        computerMove.textContent += '✊';
-        return moveNames.Rock;
+        computerMove.textContent = '✊';
+        return moves.Rock;
 
     // CASE 3B: Randomly selcted PAPER
     } else if (result === 1) {
-        computerMove.textContent += '🤚';
-        return moveNames.Paper;
+        computerMove.textContent = '🤚';
+        return moves.Paper;
 
     // CASE 3C: Randomly selected SCIZZOR
     } else if (result === 2) {
-        computerMove.textContent += '✌️';
-        return moveNames.Scizzor;
+        computerMove.textContent = '✌️';
+        return moves.Scizzor;
 
     // CASE 3D: Randomly selected LIZARD
     } else if (result === 3) {
-        computerMove.textContent += '🤏';
-        return moveNames.Lizard;
+        computerMove.textContent = '🤏';
+        return moves.Lizard;
     
     // CASE 3E: Randomly selected SPOCK
     } else {
-        computerMove.textContent += '🖖';
-        return moveNames.Spock;
+        computerMove.textContent = '🖖';
+        return moves.Spock;
+    }
+}
+/* -------------------------------------------------------------------------- */
+/**
+ * @description Listens to the click input of a mouse & records the the button 
+ *              that was pressed to the human's `score` div.
+ * @param       {object} e An object reference to the `click` button
+ * @return      {object} An enumeration from `moves`
+ */
+function getHumanMove(e) {
+
+    // STEP 1: Select the human's `choice` div
+    const humanMove = document.querySelector(`div[id="human"] div[id="choice"]`);
+
+    // CASE 2A: Player clicked ROCK
+    if (e.target.id === moves.Rock) {
+        humanMove.textContent = '✊';
+        return moves.Rock;
+
+    // CASE 2B: Player clicked PAPER
+    } else if (e.target.id === moves.Paper) {
+        humanMove.textContent = '🤚';
+        return moves.Paper;
+
+    // CASE 2C: Player clicked SCIZZOR
+    } else if (e.target.id === moves.Scizzor) {
+        humanMove.textContent = '✌️';
+        return moves.Scizzor;
+
+    // CASE 2D: Player clicked LIZARD
+    } else if (e.target.id === moves.Lizard) {
+        humanMove.textContent = '🤏';
+        return moves.Lizard;
+
+    // CASE 2E: Player clicked SPOCK
+    } else if (e.target.id === moves.Spock) {
+        humanMove.textContent = '🖖';
+        return moves.Spock;
     }
 }
 /* -------------------------------------------------------------------------- */
