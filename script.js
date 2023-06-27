3ac45249060f450d9ce742ec91f8d56d4dd7c979
Jude Thaddeau Data <78249269+jtd-117@users.noreply.github.com>
2023-06-27 21:36:30 +1000

    Add 'moves' enum

---
+++ script.js
@@ -0,0 +1,19 @@
+/**
+ * @file            Implements the logic for the 'Rock, Paper, Scizzor, 
+ *                  Lizard, Spock' game.
+ * @author          Jude Thaddeau Data
+ * @link            https://github.com/jtd-117
+ */
+/* -------------------------------------------------------------------------- */
+/**
+ * @description An ENUM for the moves the player & computer can play.
+ */
+
+const moves = Object.freeze({
+    Rock: "rock",
+    Paper: "paper",
+    Scizzor: "scizzor",
+    Lizard: "lizard",
+    Spock: "spock"
+})
+/* -------------------------------------------------------------------------- */
