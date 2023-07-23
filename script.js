b8dd87e8f4ef32f55e896f0cd8d77491a115c9a3
Jude Thaddeau Data <78249269+jtd-117@users.noreply.github.com>
2023-07-23 16:33:00 +1000

    Remove dividers & unncessary comments

--- script.js
+++ script.js
@@ -4,7 +4,8 @@
  *              Lizard, Spock' game.
  * @author      Jude Thaddeau Data
  * @link        GitHub: https://github.com/jtd-117
-* --------------------------------------------------------------------------- */
+ */
+
 /**
  * @description An ENUM for the moves the human & computer can play.
  */
@@ -16,7 +17,7 @@ const moves = Object.freeze({
     Lizard: "lizard",
     Spock: "spock"
 });
-/* -------------------------------------------------------------------------- */
+
 /**
  * @description An ENUM for keeping track of victories, losses & draws.
  */
@@ -25,7 +26,7 @@ const gameResult = Object.freeze({
     Draw: 0,
     Win: 1
 });
-/* -------------------------------------------------------------------------- */
+
 /**
  * @description Randomly generates an INTEGER that represents the computer's
  *              move according to the `moves` ENUM.
@@ -33,39 +34,31 @@ const gameResult = Object.freeze({
  */
 function getComputerMove() {
 
-    // STEP 1: Select the computer's `choice` div
     const computerMove = document.querySelector(`div[id="computer"] div[class="choice"]`);
-
-    // STEP 2: Randomly select an integer
     let result = Math.floor(Math.random() * 5);
 
-    // CASE 3A: Randomly selected ROCK
     if (result === 0) {
         computerMove.textContent = '✊';
         return moves.Rock;
 
-    // CASE 3B: Randomly selcted PAPER
     } else if (result === 1) {
         computerMove.textContent = '🤚';
         return moves.Paper;
 
-    // CASE 3C: Randomly selected SCISSOR
     } else if (result === 2) {
         computerMove.textContent = '✌️';
         return moves.Scissor;
 
-    // CASE 3D: Randomly selected LIZARD
     } else if (result === 3) {
         computerMove.textContent = '🤏';
         return moves.Lizard;
     
-    // CASE 3E: Randomly selected SPOCK
     } else {
         computerMove.textContent = '🖖';
         return moves.Spock;
     }
 }
-/* -------------------------------------------------------------------------- */
+
 /**
  * @description Listens to the click input of a mouse & records the the button 
  *              that was pressed to the human's `score` div.
@@ -74,36 +67,30 @@ function getComputerMove() {
  */
 function getHumanMove(e) {
 
-    // STEP 1: Select the human's `choice` div
     const humanMove = document.querySelector(`div[id="human"] div[class="choice"]`);
 
-    // CASE 2A: Human clicked ROCK
     if (e.target.id === moves.Rock) {
         humanMove.textContent = '✊';
         return moves.Rock;
 
-    // CASE 2B: Human clicked PAPER
     } else if (e.target.id === moves.Paper) {
         humanMove.textContent = '🤚';
         return moves.Paper;
 
-    // CASE 2C: Human clicked scissor
     } else if (e.target.id === moves.Scissor) {
         humanMove.textContent = '✌️';
         return moves.Scissor;
 
-    // CASE 2D: Human clicked LIZARD
     } else if (e.target.id === moves.Lizard) {
         humanMove.textContent = '🤏';
         return moves.Lizard;
 
-    // CASE 2E: Human clicked SPOCK
     } else if (e.target.id === moves.Spock) {
         humanMove.textContent = '🖖';
         return moves.Spock;
     }
 }
-/* -------------------------------------------------------------------------- */
+
 /**
  * @description Calculates & displays the outcome of the RPSLS round.
  * @param       {object} humanMove     
@@ -112,10 +99,9 @@ function getHumanMove(e) {
  */
 function determineOutcome(humanMove, computerMove) {
 
-    // STEP 1: Get the `reason div`
     const reasonDiv = document.getElementById('reason');
 
-    // CASE A: Human selcted ROCK
+
     if (humanMove === moves.Rock) {
 
         // CASE AI: Rock WINS against scissor & lizard
@@ -137,7 +123,7 @@ function determineOutcome(humanMove, computerMove) {
             }
             return gameResult.Lose;
         }
-    // CASE B: Human selected PAPER
+
     } else if (humanMove === moves.Paper) {
 
         // CASE BI: Paper WINS against rock & spock
@@ -164,7 +150,7 @@ function determineOutcome(humanMove, computerMove) {
             }
             return gameResult.Lose;
         }
-    // CASE C: Human selcted SCISSOR    
+
     } else if (humanMove === moves.Scissor) {
 
         // CASE CI: Scissor WINS against paper & lizard
@@ -191,7 +177,7 @@ function determineOutcome(humanMove, computerMove) {
             }
             return gameResult.Lose;
         }
-    // CASE D: Human selected LIZARD
+
     } else if (humanMove === moves.Lizard) {
 
         // CASE DI: Lizard WINS against paper & spock
@@ -218,7 +204,7 @@ function determineOutcome(humanMove, computerMove) {
             }
             return gameResult.Lose;
         }
-    // CASE E: Human selected SPOCK
+
     } else {
 
         // CASE EI: Spock WINS against rock & scissor
@@ -246,11 +232,10 @@ function determineOutcome(humanMove, computerMove) {
             return gameResult.Lose;
         }
     }
-    // CASE F: `humanMove` & `computerMove` resulted in a DRAW
     reasonDiv.textContent = `${humanMove} ties with ${computerMove}`;
     return gameResult.Draw;
 }
-/* -------------------------------------------------------------------------- */
+
 /**
  * @description Disables all HTML buttons.
  * @link        https://github.com/benfowler04/rock-paper-scissors/blob/master/rps.js
@@ -260,7 +245,7 @@ function disableButtons() {
         button.disabled = true
     });
 }
-/* -------------------------------------------------------------------------- */
+
 /**
  * @description Refreshes the website.
  * @link        https://www.freecodecamp.org/news/javascript-refresh-page-how-to-reload-a-page-in-js/#:~:text=The%20simplest%20way%20to%20refresh,and%20loading%20the%20latest%20content.
@@ -268,27 +253,23 @@ function disableButtons() {
 function refreshPage() {
     location.reload();
 }
-/* -------------------------------------------------------------------------- */
+
 /**
  * @description Plays the audio stored in the HTML tag with `id`.
  * @param       {string} id The HTML tag that contains the audio
  */
 function playAudio(id) {
 
-    // STEP 1: Locate the audio in the DOM
     const audio = document.getElementById(id);
-
-    // STEP 2: Execute the media if it exists
     if (!audio) return;
     audio.play();
 }
-/* -------------------------------------------------------------------------- */
+
 /**
  * @description Adds HTML tags to allow players to replay the game.
  */
 function playAgainPrompt() {
 
-    // STEP 1: Create 'aside' container with `h1` & `button` elements
     const replay = document.createElement('aside');
     replay.setAttribute('id', 'replay');
     const replayPrompt = document.createElement('h1');
@@ -296,7 +277,6 @@ function playAgainPrompt() {
     const replayButton = document.createElement('button');
     replayButton.setAttribute('id', 'replayButton');
 
-    // STEP 3: Style `h1` container & play victory/loss music sting
     if (humanScore === bestTo) {
         playAudio('win-mp3');
         replayPrompt.textContent = "YOU WIN!";
@@ -304,66 +284,49 @@ function playAgainPrompt() {
         playAudio('lose-mp3');
         replayPrompt.textContent = "YOU LOSE!";
     }
-    // STEP 4: Style the `button` container
+
     replayButton.textContent = "Play Again";
     replay.addEventListener('click', refreshPage);
 
-    // STEP 5: Add `h1` & `button` to `replay`
     replay.appendChild(replayPrompt);
     replay.appendChild(replayButton);
 
-    // STEP 6: Add `replay` to the end of `body`
     const body = document.querySelector('body');
     body.appendChild(replay)
 }
 
-/* -------------------------------------------------------------------------- */
 /**
  * @description Plays a single round of RPSLS.
  * @param       {object} e An object reference to the `click` button
  */
 function playRPSLS(e) {
 
-    // STEP 1: Play the button click sound
     playAudio("click-mp3");
 
-    // STEP 2: Adjust the `outcome` container
     if ((humanScore === 0) && (computerScore === 0)) {
         const resultDiv = document.getElementById('result');
         resultDiv.textContent = 'Outcome:';
     }
-    // STEP 3: Human rounds & keep track of the score
     if ((humanScore < bestTo) && (computerScore < bestTo)) {
 
-        // STEP 4: Extract `humanMove` & `computerMove`
         humanMove = getHumanMove(e);
         computerMove = getComputerMove();
-
-        // STEP 5: Determine the outcome
         result = determineOutcome(humanMove, computerMove);
 
-        // CASE 6A: The human LOST
         if (result === gameResult.Lose) {
             computerScore++;
             computerScoreDiv.textContent = `Computer: ${computerScore}`;
 
-        // CASE 6B: The human WON
         } else if (result === gameResult.Win) {
             humanScore++;
             humanScoreDiv.textContent = `Human: ${humanScore}`;
         }
-        // STEP 7: Conclude game when max score has been reached
         if ((humanScore === bestTo) || (computerScore === bestTo)) {
-
-            // STEP 8: Disable buttons
             disableButtons();
-
-            // STEP 9: Provide a mechanism to replay the game
             playAgainPrompt();
         }
     }
 }
-/* -------------------------------------------------------------------------- */
 
 // STEP 1: Define variables
 let humanMove = null;
